d9e02180f67de0ff3d1268d0ce2b83fc8ee87091
alysonvilela <alysoonvila2@gmail.com>
2022-03-22 09:11:32 -0300

    feat: turn repos, models and services into a car module

---
+++ src/modules/car/services/CreateSpecificationService.ts
@@ -0,0 +1,19 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+
+interface IRequest {
+    name: string
+    description: string
+}
+
+export class CreateSpecificationService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const alreadyExists = this.categoriesRepository.findByName(name);
+    if (alreadyExists) {
+      throw new Error('This category already exists.');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
